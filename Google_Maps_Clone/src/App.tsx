8febe6487afa50fe5ec7b3102bb1072ebf6cb5c4
Tode Temov <75773817+temov@users.noreply.github.com>
2024-11-01 15:04:12 +0100

    Add files via upload

---
+++ Google_Maps_Clone/src/App.tsx
@@ -0,0 +1,26 @@
+
+import LocationSearch from "./components/LocationSearch"
+import Map from "./components/Map"
+import type{ Place } from "./api/Place"
+import { useState } from "react"
+function App() {
+
+  const [place,setPlace] = useState<Place | null>(null)
+
+ return  (
+
+  <div className="h-screen w-screen grid grid-cols-12">
+        <div className="col-span-3 p-2">
+          <LocationSearch onPlaceClick={(p)=>setPlace(p)}/>
+        </div>
+        <div className="col-span-9">
+          <Map place={place}/>
+         </div>
+  </div>
+
+    
+ );
+ 
+}
+
+export default App;
